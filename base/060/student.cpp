759fe20b67346224a2154e481f371e3b7de39d65
David Sena <sena.ufc@gmail.com>
2021-06-22 16:46:05 -0300

    pre mirror2.0

--- base/060/student.cpp
+++ base/060/student.cpp
@@ -1,22 +1,9 @@
-#define PROF
 #include <iostream>
 #include <vector>
 using namespace std;
 
 void right_rotation(vector<int>& vet, int nrot){
-    #ifdef PROF
-        nrot = nrot % vet.size();
-        vector<int> aux;
-        for(int i = vet.size() - nrot; i < (int)vet.size(); i++)
-            aux.push_back(vet[i]);
-        for(int i = (int) vet.size() - nrot - 1; i >= 0 ; i--)
-            vet[i + nrot] = vet[i];
-        for(int i = 0; i < (int) aux.size(); i++)
-            vet[i] = aux[i];
 
-    #else
-
-    #endif
 }
 
 void show(vector<int> &vet) {
