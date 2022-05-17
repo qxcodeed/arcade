4583af7bbf9aafe988929320d8b365b9b0c925fd
David Sena <sena.ufc@gmail.com>
2022-05-17 02:43:30 -0300

    auto commit

--- base/024/Readme.md
+++ base/024/Readme.md
@@ -160,14 +160,103 @@ out:{ }
 # que desistiram
 $finish
 received: 16
-lost: 5
-tics: 6
+lost: 2
+tics: 12
 
 # end termina a simulação
 $end
 ```
 
 
+<!--FILTER solver.cpp cpp-->
+```cpp
+struct Client{
+    string name;
+    int docs;
+    int patience;
+    Client(string _name, int _actions, int _patience){
+        this->name = _name;
+        this->docs = _actions;
+        this->patience = _patience;
+    }
+    string str(){
+        return name + ":" + to_string(docs) + ":" + to_string(patience);
+    }
+};
+
+struct Banco{
+    vector<Client*> teller; //caixas
+    list<Client*> queue_in; //fila de entrada
+    list<Client*> queue_out;//fila de saida
+    int received {0};       //documentos processados
+    int lost {0};           //documentos perdidos
+    int tics {0};           //quantidade de tempo que passou
+
+
+    //iniciliza so caixas
+    Banco(int teller_size) { ... }
+
+    //insere cliente na fila de entrada
+    void insert(Client * client) { ... }
+
+    //esta vazio se não tem ninguém no banco
+    bool empty() { ... }
+
+    //roda o algoritmo de passagem de tempo
+    void tic() { ... }
+    //mostra o banco
+    void show_all(){
+        for(auto client : teller){
+            cout << "[" << (client == nullptr ? "" : client->str()) << "]";
+        }
+        cout << "\nin :{ ";
+        for(auto client : queue_in)
+            cout << client->str() << " ";
+        cout << "}\nout:{ ";
+        for(auto client : queue_out)
+            cout << client->str() << " ";
+        cout << "}\n";
+    }
+};
+
+int main(){
+    Banco banco(3);
+
+    while(true){
+        string line, cmd;
+        getline(cin, line);
+        cout << "$" << line << "\n";
+        stringstream ss(line);
+        ss >> cmd;
+        if(cmd == "end"){
+            break;
+        }
+        if(cmd == "show"){
+            banco.show_all();
+        }else if(cmd == "in"){
+            string name;
+            int docs, patience;
+            ss >> name >> docs >> patience;
+            banco.insert(new Client(name, docs, patience));
+        }else if(cmd == "init"){
+            int size;
+            ss >> size;
+            banco = Banco(size);
+        }else if(cmd == "tic"){
+            banco.tic();
+        }else if(cmd == "finish"){
+            while(!banco.empty())
+                banco.tic();
+            cout << "received: " << banco.received << endl;
+            cout << "lost: " << banco.lost - 1<< endl;
+            cout << "tics: " << banco.tics << endl;
+        }else{
+            cout << "fail: command not found\n";
+        }
+    }
+}
+```
+<!--FILTER_END-->
 
 
 
--- base/024/solver.cpp
+++ base/024/solver.cpp
@@ -1,39 +1,46 @@
+//!DEL
 #include <vector>
 #include <list>
 #include <iostream>
 #include <sstream>
 using namespace std;
 
+//!RAW
 struct Client{
     string name;
-    int actions;
+    int docs;
     int patience;
     Client(string _name, int _actions, int _patience){
         this->name = _name;
-        this->actions = _actions;
+        this->docs = _actions;
         this->patience = _patience;
     }
-    string toString(){
-        return name + ":" + to_string(actions) + ":" + to_string(patience);
+    string str(){
+        return name + ":" + to_string(docs) + ":" + to_string(patience);
     }
 };
 
 struct Banco{
-    vector<Client*> teller;
-    list<Client*> queue_in;
-    list<Client*> queue_out;
-    int received {0};
-    int lost {0};
-    int tics {0};
+    vector<Client*> teller; //caixas
+    list<Client*> queue_in; //fila de entrada
+    list<Client*> queue_out;//fila de saida
+    int received {0};       //documentos processados
+    int lost {0};           //documentos perdidos
+    int tics {0};           //quantidade de tempo que passou
 
+//!ADD
+
+    //iniciliza so caixas
     Banco(int teller_size){
-        teller = vector<Client*>(teller_size);
+        teller = vector<Client*>(teller_size, nullptr);
     }
 
+    //insere cliente na fila de entrada
     void insert(Client * client){
         queue_in.push_back(client);
     }
 
+    //esta vazio se não tem ninguém no banco
     bool empty(){
         for(auto * client : teller)
             if(client != nullptr)
@@ -41,17 +48,18 @@ struct Banco{
         return queue_in.empty() && queue_out.empty();
     }
 
+    //roda o algoritmo de passagem de tempo
     void tic(){
         tics++;
         while(!queue_out.empty()){
+            this->lost += queue_out.front()->docs;
             delete queue_out.front();
             queue_out.pop_front();
-            this->lost += 1;
         }
         for(int i = 0; i < (int) teller.size(); i++){
             if(teller[i] != nullptr){
-                if(teller[i]->actions > 0){
-                    teller[i]->actions -= 1;
+                if(teller[i]->docs > 0){
+                    teller[i]->docs -= 1;
                     this->received += 1;
                 }else{
                     queue_out.push_back(teller[i]);
@@ -75,17 +83,18 @@ struct Banco{
         }
         
     }
-
+    //!RAW
+    //mostra o banco
     void show_all(){
         for(auto client : teller){
-            cout << "[" << (client == nullptr ? "" : client->toString()) << "]";
+            cout << "[" << (client == nullptr ? "" : client->str()) << "]";
         }
         cout << "\nin :{ ";
         for(auto client : queue_in)
-            cout << client->toString() << " ";
+            cout << client->str() << " ";
         cout << "}\nout:{ ";
         for(auto client : queue_out)
-            cout << client->toString() << " ";
+            cout << client->str() << " ";
         cout << "}\n";
     }
 };
@@ -106,9 +115,9 @@ int main(){
             banco.show_all();
         }else if(cmd == "in"){
             string name;
-            int actions, patience;
-            ss >> name >> actions >> patience;
-            banco.insert(new Client(name, actions, patience));
+            int docs, patience;
+            ss >> name >> docs >> patience;
+            banco.insert(new Client(name, docs, patience));
         }else if(cmd == "init"){
             int size;
             ss >> size;
@@ -120,7 +129,7 @@ int main(){
                 banco.tic();
             cout << "received: " << banco.received << endl;
             cout << "lost: " << banco.lost - 1<< endl;
-            cout << "tics: " << banco.lost << endl;
+            cout << "tics: " << banco.tics << endl;
         }else{
             cout << "fail: command not found\n";
         }
