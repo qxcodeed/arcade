91f086a47abf88544a62699ff981144d6fc320fe
lion <sena.ufc@gmail.com>
2025-04-15 13:38:29 -0300

    treinando

--- base/treinando/.cache/draft/go/main.go
+++ base/treinando/.cache/draft/go/main.go
@@ -1,149 +1,82 @@
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-#include "lib.hpp"
-#include <sstream> //stringstream
+func tostr(vet []int) string {
+	_ = vet
+	return ""
+}
 
-namespace alu {
+func tostrrev(vet []int) string {
+	_ = vet
+	return ""
+}
 
-//==
-    std::string __tostr(int * init, int * end) {
-        if (init == end) {
-            return "";
-        }
-        return std::to_string(*init) + ", " + __tostr(init + 1, end);
-    }
+// reverse: inverte os elementos do slice
+func reverse(vet []int) {
+	_ = vet
+}
 
-    std::string tostr(int * init, int * end) {
-        std::string output = __tostr(init, end);
+// sum: soma dos elementos do slice
+func sum(vet []int) int {
+	_ = vet
+	return 0
+}
 
-        if (output.size() > 0) { //se tiver pelo menos um elemento
-            output.pop_back();   //remove o ultimo espaço
-            output.pop_back();   //remove a ultima virgula
-        }
+// mult: produto dos elementos do slice
+func mult(vet []int) int {
+	_ = vet
+	return 0
+}
 
-        return "[" + output + "]";
-    }
-//--
+// min: retorna o índice e valor do menor valor
+func min(vet []int) (int, int) {
+	_ = vet
+	return 0, 0
+}
 
-    std::string __torev(int * init, int * end) {
-        if (init == end) {
-            return "";
-        }
-        return __torev(init + 1, end) + ", " + std::to_string(*init);
-    }
+func main() {
+	var vet []int
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		if !scanner.Scan() {
+			break
+		}
+		line := scanner.Text()
+		args := strings.Fields(line)
+		fmt.Println("$" + line)
 
-//++1
-    std::string torev(int * init, int * end) {
-        std::string output = __torev(init, end);
-        if (output.size() > 0) { //se tiver pelo menos um elemento
-            output = output.substr(2); //remove os dois primeiros caracteres
-        }
-        return "[" + output + "]";
-        //ACT!
-        // (void) init;
-        // (void) end;
-        // return "";
-    }
-
-    void reverse(int * init, int * end) {
-        if (init + 1 >= end) {
-            return;
-        }
-        std::swap(*init, *(end - 1));
-        reverse(init + 1, end - 1);
-        //ACT!
-        // (void) init;
-        // (void) end;
-    }
-
-    int sum(int * init, int * end) {
-        if (init == end) {
-            return 0;
-        }
-        return *init + sum(init + 1, end);
-        //ACT!
-        // (void) init;
-        // (void) end;
-        // return 0;
-    };
-
-    int mult(int * init, int * end) {
-        if (init == end) {
-            return 1;
-        }
-        return *init * mult(init + 1, end);
-        //ACT!
-        // (void) init;
-        // (void) end;
-        // return 0;
-    };
-
-//--
-    std::pair<int, int> __min(int * init, int * end) {
-        if (init == end) {
-            return std::make_pair(-1, 0);
-        }
-        auto [ind, value] = __min(init + 1, end);
-
-        // eu sou o último
-        if (ind == -1) {
-            return {0, *init};
-        }
-
-        // eu não sou menor
-        if (*init >= value) {
-            return {ind + 1, value};
-        }
-        // eu sou menor
-        return {0, *init};
-
-    };
-//++1
-    int min(int * init, int * end) {
-        auto [ind, value] = __min(init, end);
-        return ind;
-        //ACT!
-        // (void) init;
-        // (void) end;
-        // return -1;
-    };
-
-} // namespace alu
-
-int main() {
-    std::vector<int> vet;
-
-    while (true) {
-        auto line = fn::input();
-        auto args = fn::split(line, ' ');
-        fn::write("$" + line);
-
-        if (args[0] == "end") {
-            break;
-        } else if (args[0] == "read") {
-            vet.clear();
-            for (size_t i = 1; i < args.size(); i++) {
-                vet.push_back(+args[i]);
-            }
-        } else if (args[0] == "tostr") {
-            auto resp = alu::tostr(vet.data(), vet.data() + vet.size());
-            fn::write(resp);
-        } else if (args[0] == "torev") {
-            auto resp = alu::torev(vet.data(), vet.data() + vet.size());
-            fn::write(resp);
-        } else if (args[0] == "reverse") {
-            alu::reverse(vet.data(), vet.data() + vet.size());
-        } else if (args[0] == "sum") {
-            auto resp = alu::sum(vet.data(), vet.data() + vet.size());
-            fn::write(resp);
-        } else if (args[0] == "mult") {
-            auto resp = alu::mult(vet.data(), vet.data() + vet.size());
-            fn::write(resp);
-        } else if (args[0] == "min") {
-            auto resp = alu::min(vet.data(), vet.data() + vet.size());
-            fn::write(resp);
-        } else {
-            fn::write("fail: comando invalido");
-        }
-    }
+		switch args[0] {
+		case "end":
+			return
+		case "read":
+			vet = nil
+			for _, arg := range args[1:] {
+				if val, err := strconv.Atoi(arg); err == nil {
+					vet = append(vet, val)
+				}
+			}
+		case "tostr":
+			fmt.Println(tostr(vet))
+		case "torev":
+			fmt.Println(tostrrev(vet))
+		case "reverse":
+			reverse(vet)
+		case "sum":
+			fmt.Println(sum(vet))
+		case "mult":
+			fmt.Println(mult(vet))
+		case "min":
+			_, index := min(vet)
+			fmt.Println(index)
+		default:
+			fmt.Println("fail: comando invalido")
+		}
+	}
 }
